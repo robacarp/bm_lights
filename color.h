e09a534eca54bdcaaad52c02521f8607192a1793
Robert L. Carpenter <robacarp@gmail.com>
2012-08-18 09:39:00 -0600

    screw everything about the C int-only % operator.
    weeks of my life right there.

--- color.h
+++ color.h
@@ -1,35 +1,63 @@
-  //nicked from http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+//adapted from http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsb-color-model-conversion-algorithms-in-javascript
 
 #include "Arduino.h"
+#include <math.h>
 
 class Color {
-  unsigned short red, green, blue;
 
   public:
-    void set_rgb(unsigned short* rgb){ 
+    int red, green, blue;
+
+    Color() {
+      this->red = this->green = this->blue = 100;
+    }
+
+    void set_rgb( int red, int green, int blue ){
+      int rgb[3] = {
+        (int) red,
+        (int) green,
+        (int) blue
+      };
+
+      this->set_rgb(rgb);
+    }
+
+    void set_hsb( int hue, int sat, int val) {
+      int hsb[3] = { hue, sat, val };
+      this->set_hsb(hsb);
+    }
+
+    void set_rgb(int* rgb){
       this->red = rgb[0]; this->green = rgb[1]; this->blue = rgb[2];
     }
-    void get_rgb(unsigned short* rgb){
+
+    void get_rgb(int* rgb){
       rgb[0] = this->red; rgb[1] = this->green; rgb[2] = this->blue;
     }
-    void set_hsl(unsigned short* hsl){
-      unsigned short rgb[3] = {0,0,0};
-      hsl_to_rgb(hsl,rgb);
+
+    void set_hsb(int* hsb){
+      int rgb[3] = {0,0,0};
+      hsb_to_rgb(hsb,rgb);
       set_rgb(rgb);
     }
-    void get_hsl(unsigned short* hsl){ unsigned short* rgb; get_rgb(rgb); rgb_to_hsl(rgb,hsl); }
-    void print_hsl();
+
+    void get_hsb(int* hsb){ 
+      int* rgb;
+      get_rgb(rgb);
+      rgb_to_hsb(rgb,hsb);
+    }
+
+    void print_hsb();
     void print_rgb();
 
-    //void set_hsl(unsigned short* hsl);
-    //void get_hsl(unsigned short* hsl);
-
-    void rgb_to_hsl(unsigned short* rgb, unsigned short *hsl);
-    void hsl_to_rgb(unsigned short* hsl, unsigned short *rgb);
+    void rgb_to_hsb(int* rgb, int *hsb);
+    void hsb_to_rgb(int* hsb, int *rgb);
 
 };
 
-void Color::rgb_to_hsl( unsigned short * rgb, unsigned short * hsl) {
+//not vetted
+void Color::rgb_to_hsb( int * rgb, int * hsb) {
+  /*
   float red, green, blue, min, max, chroma;
   red   = rgb[0] / 255;
   green = rgb[1] / 255;
@@ -38,15 +66,15 @@ void Color::rgb_to_hsl( unsigned short * rgb, unsigned short * hsl) {
   min = min( red, min( green, blue ) );
   chroma = max - min;
 
-  //unsigned short hue = 0, sat = 0, lum = 0;
+  //unsigned short hue = 0, sat = 0, val = 0;
 
-  //lum = (min + max) / 2;
+  //val = (min + max) / 2;
 
   if (max == min) {
     hue = sat = 0;
   } else {
     float diff = max - min;
-    if (lum > 0.5) {
+    if (val > 0.5) {
       sat = diff / ( 2 - max - min );
     } else {
       sat = diff / ( max + min );
@@ -61,40 +89,41 @@ void Color::rgb_to_hsl( unsigned short * rgb, unsigned short * hsl) {
     }
   }
 
-  //hsl[0] = hue;
-  //hsl[1] = sat;
-  //hsl[2] = lum;
+  //hsb[0] = hue;
+  //hsb[1] = sat;
+  //hsb[2] = val;
+  // */
 }
 
 
-/* hue => [0,360], sat => [0,100], lum => [0,100] */
-void Color::hsl_to_rgb( unsigned short * hsl, unsigned short * rgb) {
-  float chroma, x, sat, lum, red, green, blue;
-  int hue;
-  hue = hsl[0]; sat = hsl[1] / 100.0; lum = hsl[2] / 100.0;
+/* hue => [0,360], sat => [0,100], val => [0,100] */
+void Color::hsb_to_rgb( int * hsb, int * rgb) {
+  float r, g, b;
+  float sat = (float)hsb[1] / 100;
+  float bri = (float)hsb[2] / 100;
+  float hue = hsb[0] / 60.0;
+  float chroma = bri * sat;
+  float x;
+  x = chroma * (1 - abs( (fmod(hue, 2.0) - 1)));
 
-  //chroma = sat * ( 1- |2*lum - 1|)
-  chroma = sat * ( 1 - abs(2 * lum - 1) );
+       if (hue >= 0 && hue < 1) { r = chroma; g =  x; b = 0; }
+  else if (hue >= 1 && hue < 2) { r = x; g =  chroma; b = 0; }
+  else if (hue >= 2 && hue < 3) { r = 0; g =  chroma; b = x; }
+  else if (hue >= 3 && hue < 4) { r = 0; g =  x; b = chroma; }
+  else if (hue >= 4 && hue < 5) { r = x; g =  0; b = chroma; }
+  else if (hue >= 5 && hue < 6) { r = chroma; g =  0; b = x; }
+     else { rgb[0] = 0; rgb[1] = 0; rgb[2] = 0; }
 
-  //hue' = hue / 60
-  hue = hsl[0] / 60;
+  x = bri - chroma;
+  r += x;
+  g += x;
+  b += x;
 
-  //x = chroma * (1- |hue' % 2-1|)
-  x = chroma * ( 1- abs(hue % 2 -1) );
-
-  if      (hue >= 0 && hue < 1) { red = chroma;  green = x;       blue = 0;      }
-  else if (hue >= 1 && hue < 2) { red = x;       green = chroma;  blue = 0;      }
-  else if (hue >= 2 && hue < 3) { red = 0;       green = chroma;  blue = x;      }
-  else if (hue >= 3 && hue < 4) { red = 0;       green = x;       blue = chroma; }
-  else if (hue >= 4 && hue < 5) { red = x;       green = 0;       blue = chroma; }
-  else if (hue >= 5 && hue < 6) { red = chroma;  green = 0;       blue = x;      }
-  else                          { red = 0;       green = 0;       blue = 0;      }
-
-  x = lum - chroma * 0.5;
-  rgb[0] = 255 * (red + x);
-  rgb[1] = 255 * (green + x);
-  rgb[2] = 255 * (blue + x);
+  rgb[0] = r * 255;
+  rgb[1] = g * 255;
+  rgb[2] = b * 255;
 }
+
 
 void Color::print_rgb(){
   Serial.print("red: ");
@@ -105,16 +134,16 @@ void Color::print_rgb(){
   Serial.println( blue );
 }
 
-void Color::print_hsl(){
-  unsigned short hsl[3];
-  unsigned short rgb[3] = { red, green, blue };
+void Color::print_hsb(){
+  int hsb[3];
+  int rgb[3] = { red, green, blue };
 
-  rgb_to_hsl(rgb,hsl);
+  rgb_to_hsb(rgb,hsb);
 
   Serial.print("hue: ");
-  Serial.print(hsl[0]);
+  Serial.print(hsb[0]);
   Serial.print(" sat: ");
-  Serial.print(hsl[1]);
-  Serial.print(" lum: ");
-  Serial.println(hsl[2]);
+  Serial.print(hsb[1]);
+  Serial.print(" val: ");
+  Serial.println(hsb[2]);
 }
